a6f0845fc022b08c23ae8f51a900180ba3170866
James Wong <jameswong8@gmail.com>
2017-12-22 01:05:03 -0500

    port

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 from routes import *
 
 if __name__ == '__main__':
-	app.run(debug=True, port=5000)
+	port = os.environ.get('PORT', 5000)
+	app.run(debug=True, port=port)
 	
